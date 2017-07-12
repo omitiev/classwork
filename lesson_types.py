c89d7bd2c98448aed08db7f96a7d8bc8bb03f91e
oleksii <oleksii.mitiev@gmail.com>
2017-07-12 18:58:25 +0300

    test

--- lesson_types.py
+++ lesson_types.py
@@ -22,4 +22,26 @@ catheter_b=4
 hypotenuse= math.sqrt((catheter_a**2)+(catheter_b**2))
 print('hypotenuse =', round (hypotenuse, 2))
 print("hypotenuse for traingle with catheter_a %.3f and catheter_b %.3f is - %.3f" % (catheter_a, catheter_b, hypotenuse))
-test
+
+
+s = '\u26bd'
+print("symbol %s has unicode -  %d" % (s , ord(s)))
+
+code=0x26BD
+print("unicode of %d has symbol - %s" % (code , chr(code)))
+
+
+#       01234567
+time = "18:44:42"
+hours = float(time[:2])
+print(hours)
+minutes = float(time[3:5])
+print(minutes)
+seconds = float(time[6:])
+print(seconds)
+total_seconds=hours*3600 + minutes*60 + seconds
+print(total_seconds)
+
+lst=time.split(":")
+print(lst)
+
