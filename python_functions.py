e1d49a7710fcb5f18a871b8ed11cbd5af94b69bd
oleksii <oleksii.mitiev@gmail.com>
2017-07-16 18:20:12 +0300

    test

---
+++ python_functions.py
@@ -0,0 +1,17 @@
+
+#-----------------------------------------------
+
+def pow(a, b):
+    print("I'm inside pow function!")
+    return a**b
+
+result = pow(2, 3)
+print(result)
+
+#-----------------------------------------------
+def rectangle_square (h, w):
+    result = h*w
+    return result
+
+result2 = rectangle_square(10, 20)
+print(result2)
