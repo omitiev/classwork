630ff1c2a0247a4913261909858e310c294d1b98
oleksii <oleksii.mitiev@gmail.com>
2017-07-23 17:36:27 +0300

    intermediate commit

---
+++ lesson_loops.py
@@ -0,0 +1,19 @@
+for i in range(5):
+    print("Hello world", i)
+print(i)
+
+# for i in range(101):
+#     print(i)
+
+for i in range(101):
+    if i%2 == 0:
+        print(i)
+    # else:
+    #     continue
+
+
+# i = 1
+# while i<=100:
+#     print("Hello world", i)
+#     i = i+1
+
