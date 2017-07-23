a997279bc29eebd71dbca927dfa68f7f29bdc14c
oleksii <oleksii.mitiev@gmail.com>
2017-07-23 18:02:10 +0300

    intermediate commit

--- lesson_loops.py
+++ lesson_loops.py
@@ -1,3 +1,5 @@
+import lesson_conditions
+
 for i in range(5):
     print("Hello world", i)
 print(i)
@@ -8,8 +10,6 @@ print(i)
 for i in range(101):
     if i%2 == 0:
         print(i)
-    # else:
-    #     continue
 
 
 # i = 1
@@ -17,3 +17,19 @@ for i in range(101):
 #     print("Hello world", i)
 #     i = i+1
 
+for i in range(2100):
+    if lesson_conditions.is_leap_year(i):
+        print("Leap year", i)
+
+# range - function which creates the range from 0 up to set value-1
+# r = range(10)
+# r
+# range (0,10)
+# list (r)
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for i in range(0,10,2):
+    print(i)
+
+for i in "abcdefghijkl":
+    print(i)
