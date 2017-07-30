d3745feb95ce5c5040e2039e581dc925e1d88c27
oleksii <oleksii.mitiev@gmail.com>
2017-07-30 18:22:43 +0300

    intermediate commit

---
+++ lesson_nested_loops.py
@@ -0,0 +1,19 @@
+# for i in range(10):
+#     for j in range(10):
+#         for k in range(10):
+
+for i in range(10):
+    print("%d: " % i, end="")
+    for j in range(10):
+        print("%d" % j, end="")
+    print()
+
+
+def pyph_table(n=9, m=9):
+    for i in range(1, n+1):
+        # print("%d: " % i, end="")
+        for j in range(1, m+1):
+            print("%d" % (i*j), end="\t")
+        print()
+
+pyph_table(2, 5)
