88dbabf6832c6916480b42acb962201f69dcc595
oleksii <oleksii.mitiev@gmail.com>
2017-07-30 17:26:30 +0300

    intermediate commit

---
+++ lesson_lists.py
@@ -0,0 +1,15 @@
+lst = [10, 20, 30, 40, 50, 60, 70]
+print(lst[0])
+print(lst[1])
+print(lst[2])
+print('------')
+for i in range (3):
+    print(lst[i])
+
+print('------')
+for i in range (2, 7):
+    print(lst[i])
+
+print('------')
+for i in range (len(lst)):
+    print(lst[i])
