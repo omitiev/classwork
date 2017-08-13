c9987c6f770936f09e5484ae79554e95fc09009d
oleksii <oleksii.mitiev@gmail.com>
2017-08-13 17:50:14 +0300

    intermediate commit

---
+++ lesson_classes.py
@@ -0,0 +1,8 @@
+import lesson_utils
+
+
+
+martix = [[0]*5 for i in range(3)]
+lesson_utils.print_matrix(matrix)
+
+
---
+++ lesson_utils.py
@@ -0,0 +1,5 @@
+def print_matrix(matrix):
+    for row in matrix:
+        for elem in row:
+            print(elem, end='\t')
+            print()
