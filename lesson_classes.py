c416fdaea2d7a8407a800a81bb8a2f136df5a04f
oleksii <oleksii.mitiev@gmail.com>
2017-08-13 18:12:14 +0300

    intermediate commit

--- lesson_classes.py
+++ lesson_classes.py
@@ -1,8 +1,13 @@
-import lesson_utils
+# import lesson_utils
+from lesson_utils import print_matrix as my_print_matrix
+import sys
+import pprint
 
 
+matrix = [[0]*5 for i in range(3)]
+print_matrix = True
+if print_matrix:
+    my_print_matrix(matrix)
 
-martix = [[0]*5 for i in range(3)]
-lesson_utils.print_matrix(matrix)
-
+pprint.pprint(sys.path)
 
