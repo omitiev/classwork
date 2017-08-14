2f61aa61c67093b752eab8961604c215d6b4a175
oleksii <oleksii.mitiev@gmail.com>
2017-08-14 19:43:18 +0300

    intermediate commit

--- lesson_list_hacks.py
+++ lesson_list_hacks.py
@@ -102,9 +102,9 @@ print((type(var)))
 
 def fibonacci_number(n):
     if n == 0:
-        return  1
-    elif n ==1:
-        return  1
+        return 1
+    elif n == 1:
+        return 1
     else:
         return fibonacci_number(n-1) + fibonacci_number(n-2)
 
