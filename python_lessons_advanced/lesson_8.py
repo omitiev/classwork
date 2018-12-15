3f779d703771b7bc59fddb542bf478adae7c9606
oleksii <oleksii.mitiev@gmail.com>
2018-12-15 14:01:15 +0200

    lesson_8 commit

--- python_lessons_advanced/lesson_8.py
+++ python_lessons_advanced/lesson_8.py
@@ -13,8 +13,53 @@ class MySupress:
         #     return True
 
         # 2
-        # if isinstance(self.exception_type(), exc_type):
+        # if isinstance(exc_type(), self.exception_type):
         #     return True
+        # ==
+        # return isinstance(exc_type(), self.exception_type)
 
 with MySupress(KeyError):
     {}['key']
+
+with MySupress(IndexError):
+    [][1]
+
+with MySupress(Exception):
+    {}['key']
+
+
+# class Opened:
+#     def __init__(self, file_name, mode):
+#         self.handler = open(file_name, mode)
+#
+#     def __enter__(self):
+#         return self.handler
+#
+#     def __exit__(self, exc_type, exc_val, exc_tb):
+#         self.handler.close()
+#         del self.handler
+
+class Opened:
+    def __init__(self, file_name, mode):
+        self.file_name = file_name
+        self.mode = mode
+        if not self.file_name or self.mode:
+            raise ValueError("name: {} or mode: {} shouldn't be empty".format(
+                self.file_name,
+                self.mode
+            ))
+
+    def __enter__(self):
+        self.handler = open(self.file_name, self.mode)
+        return self.handler
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.handler.close()
+
+
+
+with Opened("file_name.txt", mode="a") as o_file:
+    o_file.write("hello")
+#
+# with Opened("", mode="a") as o_file:
+#     o_file.write("hello")
